d0fdbeb3b876d179495ed39b9f928b64a75b5bef
Sylver-Icy <abhinavsingh.codes@gmail.com>
2025-12-01 15:15:28 +0530

    feat: gain energy for lvl up

--- utils/levelup.py
+++ utils/levelup.py
@@ -1,5 +1,7 @@
-from services.response_services import create_response
 import asyncio
+
+from services.response_services import create_response
+from services.jobs_services import JobsClass
 
 async def give_exp(ctx, user_id: int, exp_amount: int):
     """Adds exp while also checking if level up happened"""
@@ -7,4 +9,6 @@ async def give_exp(ctx, user_id: int, exp_amount: int):
     leveled_up = await asyncio.to_thread(add_exp, user_id, exp_amount)
 
     if leveled_up:
+        user = JobsClass(user_id)
+        user.gain_energy(15)
         await ctx.send(create_response("level_up",1,user=ctx.author.mention,level=leveled_up))
